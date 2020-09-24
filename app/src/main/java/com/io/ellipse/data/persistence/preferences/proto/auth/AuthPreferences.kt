c5fd04f1fc95579b1a501a52356dccebc77b31e3
dimitreuz <dimitreuzsokolov@gmail.com>
2020-09-24 03:47:20 +0300

    Added authentication components to store access token locally using cipher.

---
+++ app/src/main/java/com/io/ellipse/data/persistence/preferences/proto/auth/AuthPreferences.kt
@@ -0,0 +1,33 @@
+package com.io.ellipse.data.persistence.preferences.proto.auth
+
+import android.content.Context
+import androidx.datastore.createDataStore
+import com.io.ellipse.data.persistence.preferences.proto.BasePreferences
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+class AuthPreferences @Inject constructor(
+    @ApplicationContext context: Context,
+    serializer: AuthModelSerializer
+) : BasePreferences<AuthModel>(
+    context.createDataStore(
+        fileName = DATAFILE_PATH,
+        serializer = serializer,
+        scope = CoroutineScope(Dispatchers.IO)
+    )
+) {
+
+    companion object {
+        private const val DATAFILE_PATH = "auth_preferences.pb"
+    }
+
+    suspend fun setAuthToken(authToken: String) = updateData {
+        it.toBuilder().setAuthorizationToken(authToken).build()
+    }
+
+    suspend fun setRefreshToken(refreshToken: String) = updateData {
+        it.toBuilder().setAuthorizationToken(refreshToken).build()
+    }
+}
